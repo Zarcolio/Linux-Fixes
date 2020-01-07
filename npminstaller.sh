a90be7fc9a3fe355e1c14222346ea3f1cac54a05
Ocraz <registered@quicknet.nl>
2020-01-07 13:09:44 +0100

    Add files via upload

---
+++ npminstaller.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+sudo apt-get install python g++ make checkinstall fakeroot -y
+src=$(mktemp -d) && cd $src
+wget -N http://nodejs.org/dist/node-latest.tar.gz
+tar xzvf node-latest.tar.gz && cd node-v*
+./configure
+sudo fakeroot checkinstall -y --install=no --pkgversion $(echo $(pwd) | sed -n -re's/.+node-v(.+)$/\1/p') make -j$(($(nproc)+1)) install
